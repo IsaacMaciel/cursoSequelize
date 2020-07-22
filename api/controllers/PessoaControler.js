5fb6a44a47917b84a2dcde959e60c9a3964b09a4
Isaac Maciel de Araújo Neto <Isaac.neto@mcmmontagens.com.br>
2020-07-22 16:09:52 -0300

    alteração do inicializador do sequelize

--- api/controllers/PessoaControler.js
+++ api/controllers/PessoaControler.js
@@ -1,11 +1,9 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize();
-const database = require("../models/pessoas")(sequelize, DataTypes);
+const database = require("../models");
 
 class PessoaController {
     static async getAllPeople(req, res) {
         try {
-            const getAllPeople = await database.findAll();
+            const getAllPeople = await database.Pessoa.findAll();
             return res.status(200).json(getAllPeople);
         } catch (error) {
             return res.status(500).json(error.message);
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,6 @@
 const bodyParser = require("body-parser");
 const pessoas = require("./pessoasRouter");
 
-const PessoaControler = require("../controllers/PessoaControler");
-
 module.exports = (app) => {
     app.use(bodyParser.json());
     app.use(pessoas);
